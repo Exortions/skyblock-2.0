194f5d567bf7680f8472102075f7097369c355df
Exortions <exortionsmc@gmail.com>
2022-11-15 15:21:14 -0800

    add util functions to main class

--- src/main/java/com/skyblock/skyblock/Skyblock.java
+++ src/main/java/com/skyblock/skyblock/Skyblock.java
@@ -1,5 +1,6 @@
 package com.skyblock.skyblock;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("unused")
@@ -13,5 +14,13 @@ public final class Skyblock extends JavaPlugin {
     @Override
     public void onDisable() {
 
+    }
+
+    public String getVersion() {
+        return this.getDescription().getVersion();
+    }
+
+    public String getPrefix() {
+        return ChatColor.translateAlternateColorCodes('&', "&7[&3S&bB&7] &f");
     }
 }
