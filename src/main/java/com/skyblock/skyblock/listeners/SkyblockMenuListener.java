f1b8c3137c49f2a222f25b538322d452eabc0174
Exortions <exortionsmc@gmail.com>
2022-11-16 20:47:38 -0800

    skyblock menu

--- src/main/java/com/skyblock/skyblock/listeners/SkyblockMenuListener.java
+++ src/main/java/com/skyblock/skyblock/listeners/SkyblockMenuListener.java
@@ -9,8 +9,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -30,7 +33,66 @@ public class SkyblockMenuListener implements Listener {
     public void registerGui() {
         Gui skyblockMenu = new Gui(MENU_NAME, 54, new HashMap<>());
 
-        skyblockMenu.addItem(19, new ItemBuilder(ChatColor.GREEN + "Your Skills", Material.DIAMOND_SWORD).addLore(ChatColor.GRAY + "View your Skill progression and", ChatColor.GRAY + "rewards.", "", ChatColor.GOLD + "14.8 Skill Avg. " + ChatColor.DARK_GRAY + "(non-cosmetic)", "", ChatColor.YELLOW + "Click to view!").toItemStack());
+        skyblockMenu.addItem(13, new ItemBuilder(
+                ChatColor.GREEN + "Your SkyBlock Profile",
+                Material.SKULL_ITEM,
+                1, (short) 3)
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES)
+                .addLore(
+                        ChatColor.GRAY + "View your equipment, stats,",
+                        ChatColor.GRAY + "and more!",
+                        "",
+                        ChatColor.RED + " ❤ Health " + ChatColor.WHITE + "100",
+                        ChatColor.GREEN + " ❈ Defence " + ChatColor.WHITE + "0",
+                        ChatColor.RED + " ❁ Strength " + ChatColor.WHITE + "0",
+                        ChatColor.WHITE + " ✦ Speed " + ChatColor.WHITE + "0",
+                        ChatColor.BLUE + " ☣ Crit Chance " + ChatColor.WHITE + "0",
+                        ChatColor.BLUE + " ☠ Crit Damage " + ChatColor.WHITE + "0",
+                        ChatColor.AQUA + " ✎ Intelligence " + ChatColor.WHITE + "0",
+                        ChatColor.YELLOW + " ⚔ Attack Speed " + ChatColor.WHITE + "0",
+                        ChatColor.DARK_AQUA + " ∞ Sea Creature Chance " + ChatColor.WHITE + "0",
+                        ChatColor.AQUA + " ✯ Magic Find " + ChatColor.WHITE + "0",
+                        ChatColor.LIGHT_PURPLE + " ♣ Pet Luck " + ChatColor.WHITE + "0",
+                        ChatColor.WHITE + " ❂ True Defense " + ChatColor.WHITE + "0",
+                        ChatColor.RED + " ⫽ Ferocity " + ChatColor.WHITE + "0",
+                        ChatColor.RED + " ✹ Ability Damage " + ChatColor.WHITE + "0",
+                        ChatColor.GOLD + " ☘ Mining Fortune " + ChatColor.WHITE + "0",
+                        ChatColor.GOLD + " ⸕ Mining Speed " + ChatColor.WHITE + "0",
+                        "",
+                        ChatColor.YELLOW + "Click to view!"
+                ).toItemStack());
+
+        skyblockMenu.addItem(19, new ItemBuilder(
+                ChatColor.GREEN + "Your Skills",
+                Material.DIAMOND_SWORD)
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE)
+                .addLore(
+                        ChatColor.GRAY + "View your Skill progression and",
+                        ChatColor.GRAY + "rewards.", "",
+                        ChatColor.GOLD + "14.8 Skill Avg. " + ChatColor.DARK_GRAY + "(non-cosmetic)",
+                        "",
+                        ChatColor.YELLOW + "Click to view!")
+                .toItemStack());
+
+        skyblockMenu.addItem(20, new ItemBuilder(
+                ChatColor.GREEN + "Collection",
+                Material.ITEM_FRAME)
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE)
+                .addLore(
+                        ChatColor.GRAY + "View all of the items available",
+                        ChatColor.GRAY + "in SkyBlock. Collect more of an",
+                        ChatColor.GRAY + "item to unlock rewards on your",
+                        ChatColor.GRAY + "way to becoming a master of",
+                        ChatColor.GRAY + "SkyBlock!",
+                        "",
+                        ChatColor.GRAY + "Collection Unlocked: " + ChatColor.YELLOW + "82.1" + ChatColor.GOLD + "%",
+                        ChatColor.DARK_GREEN + "" + ChatColor.BOLD + "⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯" + ChatColor.WHITE + ChatColor.BOLD + "⎯⎯⎯⎯ " + ChatColor.YELLOW + "55" + ChatColor.GOLD + "/" + ChatColor.YELLOW + "67",
+                        "",
+                        ChatColor.YELLOW + "Click to view!"
+                )
+                .toItemStack());
+
+        skyblockMenu.fillEmpty(new ItemBuilder(EMPTY_NAME, Material.STAINED_GLASS_PANE, (short) 15).toItemStack());
 
         skyblock.getGuiHandler().registerGui("skyblock_menu", skyblockMenu);
     }
--- src/main/java/com/skyblock/skyblock/utilities/gui/Gui.java
+++ src/main/java/com/skyblock/skyblock/utilities/gui/Gui.java
@@ -3,16 +3,20 @@ package com.skyblock.skyblock.utilities.gui;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 public class Gui implements Listener {
@@ -35,7 +39,23 @@ public class Gui implements Listener {
         Inventory inventory = player.getServer().createInventory(null, slots, name);
 
         for (int i = 0; i < slots; i++) {
-            if (items.containsKey(i)) inventory.setItem(i, items.get(i));
+            if (items.containsKey(i)) {
+                if (Objects.equals(this.getName(), "Skyblock Menu") && items.get(i).getType().equals(Material.SKULL_ITEM) && items.get(i).getItemMeta().getDisplayName().equals(ChatColor.GREEN + "Your SkyBlock Profile")) {
+                    ItemStack stack = items.get(i);
+
+                    SkullMeta meta = (SkullMeta) stack.getItemMeta();
+
+                    meta.setOwner(player.getName());
+
+                    stack.setItemMeta(meta);
+
+                    inventory.setItem(i, stack);
+
+                    continue;
+                }
+
+                inventory.setItem(i, items.get(i));
+            }
         }
 
         player.openInventory(inventory);
@@ -50,8 +70,8 @@ public class Gui implements Listener {
     }
 
     public void fillEmpty(ItemStack stack) {
-        for (int i = 0; i < this.items.size(); i++) {
-            this.items.putIfAbsent(i, stack);
+        for (int i = 0; i < this.slots; i++) {
+            if (!this.items.containsKey(i)) this.items.put(i, stack);
         }
     }
 
