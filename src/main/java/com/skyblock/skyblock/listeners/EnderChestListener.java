c5b29d0ddc716054f70a4079b28527385c2bfec3
Exortions <exortionsmc@gmail.com>
2022-11-20 10:08:42 -0800

    add sounds to ender chest

--- src/main/java/com/skyblock/skyblock/listeners/EnderChestListener.java
+++ src/main/java/com/skyblock/skyblock/listeners/EnderChestListener.java
@@ -1,5 +1,7 @@
 package com.skyblock.skyblock.listeners;
 
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
@@ -11,6 +13,8 @@ public class EnderChestListener implements Listener {
         if (!event.getInventory().getTitle().equals("Ender Chest")) return;
 
         event.getPlayer().getEnderChest().setContents(event.getInventory().getContents());
+
+        ((Player) event.getPlayer()).playSound(event.getPlayer().getLocation(), Sound.CHEST_CLOSE, 1, 1);
     }
 
 }
