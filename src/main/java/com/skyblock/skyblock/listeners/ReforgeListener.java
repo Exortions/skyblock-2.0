3800ef01930a9f67282aac315b3cb0ff313d97e4
Exortions <exortionsmc@gmail.com>
2022-11-22 08:11:31 -0800

    blacksmith validation finally fixed + bug fixes

--- src/main/java/com/skyblock/skyblock/listeners/ReforgeListener.java
+++ src/main/java/com/skyblock/skyblock/listeners/ReforgeListener.java
@@ -13,7 +13,6 @@ import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class ReforgeListener implements Listener {
@@ -23,34 +22,69 @@ public class ReforgeListener implements Listener {
         if (!(event.getWhoClicked() instanceof Player)) return;
 
         if (event.getClickedInventory() == null) return;
-        if ((event.getCurrentItem() == null && event.getCursor() == null)) return;
-        if (event.getCurrentItem() == null && event.getCursor() == null) return;
         if (!event.getClickedInventory().getTitle().equals("Reforge Item")) return;
 
         Player player = (Player) event.getWhoClicked();
+
+        if ((event.getCurrentItem() == null && event.getCursor() == null)) return;
+        if (event.getCurrentItem() == null && event.getCursor() == null) return;
 
         ItemStack item = event.getCurrentItem();
 
         event.setCancelled(event.getSlot() != 13);
 
-        if (event.getSlot() == 13) {
-            if (event.getCursor() == null || event.getCursor().getType().equals(Material.AIR)) return;
+        if (
+                event.getAction().equals(InventoryAction.PICKUP_ALL)
+                || event.getAction().equals(InventoryAction.PLACE_ALL)
+                || event.getAction().equals(InventoryAction.PICKUP_ONE)
+                || event.getAction().equals(InventoryAction.PICKUP_HALF)
+                || event.getAction().equals(InventoryAction.PICKUP_SOME)
+                || event.getAction().equals(InventoryAction.PLACE_ONE)
+                || event.getAction().equals(InventoryAction.PLACE_SOME)
+        ) {
+            if (event.getSlot() == 13) {
+                boolean isPickup = event.getAction().equals(InventoryAction.PICKUP_ALL)
+                        && !event.getAction().equals(InventoryAction.PICKUP_ONE)
+                        && !event.getAction().equals(InventoryAction.PICKUP_HALF)
+                        && !event.getAction().equals(InventoryAction.PICKUP_SOME);
 
-            boolean canReforge = Util.isItemReforgeable(event.getCursor());
+                event.setCancelled(
+                        !isPickup
+                );
 
-            if (canReforge) {
-                event.getClickedInventory().setItem(22, new ItemBuilder(ChatColor.YELLOW + "Reforge Item", Material.ANVIL).addLore(Util.buildLore("&7Place an item above to reforge\n&7it! Reforging items adds a\n&7random modifier to the item that\n&7grants stat boosts.")).addNBT("reforge", true).toItemStack());
-            } else {
-                event.getClickedInventory().setItem(22, new ItemBuilder(ChatColor.RED + "Error!", Material.BARRIER).addLore(Util.buildLore("&7You cannot reforge this item!")).addNBT("reforge", true).toItemStack());
+                ItemStack old = event.getCurrentItem();
+                ItemStack newItem = event.getCursor();
+
+                if (newItem == null) return;
+
+                boolean isReforgeable = true;
+
+                if (!newItem.getType().equals(Material.AIR)) isReforgeable = Util.isItemReforgeable(newItem);
+
+                ItemStack reforgeItem =
+                        isReforgeable || newItem.getType().equals(Material.AIR) ?
+                                new ItemBuilder(ChatColor.YELLOW + "Reforge Item", Material.ANVIL)
+                                        .addLore(Util.buildLore("&7Place an item above to reforge\n&7it! Reforging items adds a\n&7random modifier to the item that\n&7grants stat boosts."))
+                                        .toItemStack() :
+                                new ItemBuilder(ChatColor.RED + "Error!", Material.BARRIER)
+                                        .addLore(Util.buildLore("&7You cannot reforge this item!"))
+                                        .toItemStack();
+
+                event.getClickedInventory().setItem(22, reforgeItem);
+                event.getClickedInventory().setItem(13, newItem);
+
+                player.updateInventory();
+
+                if (!isPickup) player.setItemOnCursor(old);
+                else {
+                    player.getInventory().addItem(old);
+
+                    player.updateInventory();
+                }
+
+
+                return;
             }
-
-            event.setCancelled(false);
-
-            InventoryClickEvent updater = new InventoryClickEvent(event.getView(), InventoryType.SlotType.CONTAINER, 1, ClickType.LEFT, event.getAction(), event.getHotbarButton());
-
-            Bukkit.getPluginManager().callEvent(updater);
-
-            return;
         }
 
         NBTItem nbt = new NBTItem(item);
