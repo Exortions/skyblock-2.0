52f0e61fe36e21a2941b687bef59930ef4c4301f
Exortions <exortionsmc@gmail.com>
2022-11-28 00:21:17 -0500

    cleanup npc code

--- src/main/java/com/skyblock/skyblock/features/npc/NPCHandler.java
+++ src/main/java/com/skyblock/skyblock/features/npc/NPCHandler.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 
 public class NPCHandler {
 
-    private HashMap<String, NPC> npcs;
+    private final HashMap<String, NPC> npcs;
 
     public NPCHandler() {
         this.npcs = new HashMap<>();
@@ -37,25 +37,8 @@ public class NPCHandler {
         this.npcs.put(id, npc);
     }
 
-    public NPC getNPC(String id) {
-        return this.npcs.get(id);
-    }
-
-    public NPC getNPCByName(String name) {
-        for (NPC npc : this.npcs.values()) {
-            if (npc.getName().equalsIgnoreCase(name)) {
-                return npc;
-            }
-        }
-        return null;
-    }
-
     public HashMap<String, NPC> getNPCs() {
         return this.npcs;
-    }
-
-    public void removeNPC(String id) {
-        this.npcs.remove(id);
     }
 
 }
