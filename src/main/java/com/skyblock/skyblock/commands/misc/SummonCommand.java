4a05923a19d763322866889fc5f68cd8faeef203
Exortions <exortionsmc@gmail.com>
2022-12-16 14:44:23 -0800

    update summon command

--- src/main/java/com/skyblock/skyblock/commands/misc/SummonCommand.java
+++ src/main/java/com/skyblock/skyblock/commands/misc/SummonCommand.java
@@ -7,18 +7,27 @@ import com.skyblock.skyblock.utilities.command.annotations.Alias;
 import com.skyblock.skyblock.utilities.command.annotations.Description;
 import com.skyblock.skyblock.utilities.command.annotations.RequiresPlayer;
 import com.skyblock.skyblock.utilities.command.annotations.Usage;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 @RequiresPlayer
 @Alias(aliases = { "spawn" })
-@Usage(usage = "/sb summon <mobname>")
+@Usage(usage = "/sb summon <type> <name> [amount]")
 @Description(description = "Summon a custom mob.")
 public class SummonCommand implements Command {
 
     @Override
     public void execute(Player player, String[] args, Skyblock plugin) {
-        if (args.length == 2) {
-            SkyblockEntityType.valueOf(args[0].toUpperCase()).getNewInstance(args[1]).spawn(player.getLocation());
+        if (args.length >= 2) {
+            int amount = args.length == 3 ? Integer.parseInt(args[2]) : 1;
+
+            for (int i = 0; i < amount; i++) {
+                try {
+                    SkyblockEntityType.valueOf(args[0].toUpperCase()).getNewInstance(args[1]).spawn(player.getLocation());
+                } catch (NullPointerException ex) {
+                    player.sendMessage(ChatColor.RED + "Could not find mob with identifier " + ChatColor.DARK_GRAY + "skyblock:" + args[0].toLowerCase() + "." + args[1].toLowerCase() + ChatColor.RED + "!");
+                }
+            }
         }
     }
 }
