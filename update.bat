d68ecd3051b6c4a725ca4a6cf3f7a4af94826a4d
Exortions <exortionsmc@gmail.com>
2022-12-15 21:42:37 -0800

    remove update.bat

--- update.bat
+++
@@ -1,6 +0,0 @@
-echo f | xcopy /K /D /H /Y /S .\dependencies\skyblock\merchants.json .\server\plugins\Skyblock\merchants.json
-echo f | xcopy /K /D /H /Y /S .\dependencies\skyblock\collections\*.json .\server\plugins\Skyblock\collections\
-echo f | xcopy /K /D /H /Y /S .\dependencies\skyblock\launchpads.yml .\server\plugins\Skyblock\launchpads.yml
-echo f | xcopy /K /D /H /Y /S .\dependencies\skyblock\locations.yml .\server\plugins\Skyblock\locations.yml
-echo f | xcopy /K /D /H /Y /S .\dependencies\skyblock\private_island.schematic .\server\plugins\Skyblock\private_island.schematic
-echo f | xcopy /K /D /H /Y /S .\dependencies\skyblock\reforges.json .\server\plugins\Skyblock\reforges.json
