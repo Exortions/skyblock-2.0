ba78a3ba9457020209e3e91c90849333064e9470
Exortions <exortionsmc@gmail.com>
2022-12-18 21:50:01 -0800

    fix bugs (protection enchantment)

--- src/main/java/com/skyblock/skyblock/features/enchantment/enchantments/armor/ProtectionEnchantment.java
+++ src/main/java/com/skyblock/skyblock/features/enchantment/enchantments/armor/ProtectionEnchantment.java
@@ -1,15 +1,8 @@
 package com.skyblock.skyblock.features.enchantment.enchantments.armor;
 
-import com.inkzzz.spigot.armorevent.PlayerArmorEquipEvent;
-import com.inkzzz.spigot.armorevent.PlayerArmorUnequipEvent;
-import com.skyblock.skyblock.SkyblockPlayer;
-import com.skyblock.skyblock.enums.SkyblockStat;
 import com.skyblock.skyblock.features.enchantment.types.ArmorEnchantment;
 import com.skyblock.skyblock.utilities.item.ItemBase;
 import org.bukkit.ChatColor;
-import org.bukkit.event.EventHandler;
-
-import java.util.function.Function;
 
 public class ProtectionEnchantment extends ArmorEnchantment {
     public ProtectionEnchantment() {
