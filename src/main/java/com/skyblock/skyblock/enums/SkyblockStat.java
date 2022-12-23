619c2dc38facd268775c648a33f6baa4a0b4a2a1
Exortions <exortionsmc@gmail.com>
2022-12-22 22:38:16 -0800

    add skyblock stat icons

--- src/main/java/com/skyblock/skyblock/enums/SkyblockStat.java
+++ src/main/java/com/skyblock/skyblock/enums/SkyblockStat.java
@@ -1,24 +1,33 @@
 package com.skyblock.skyblock.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum SkyblockStat {
 
-    STRENGTH,
-    HEALTH,
-    MAX_HEALTH,
-    SPEED,
-    MANA,
-    MAX_MANA,
-    MINING_SPEED,
-    ATTACK_SPEED,
-    CRIT_CHANCE,
-    CRIT_DAMAGE,
-    PET_LUCK,
-    MAGIC_FIND,
-    SEA_CREATURE_CHANCE,
-    TRUE_DEFENSE,
-    DEFENSE,
-    FEROCITY,
-    ABILITY_DAMAGE,
-    DAMAGE,
+    STRENGTH("❁"),
+    HEALTH("❤"),
+    MAX_HEALTH(""),
+    SPEED("✦"),
+    MANA("✎"),
+    MAX_MANA(""),
+    MINING_SPEED("⸕"),
+    ATTACK_SPEED("⚔"),
+    CRIT_CHANCE("☣"),
+    CRIT_DAMAGE("☠"),
+    PET_LUCK("♣"),
+    MAGIC_FIND("✯"),
+    SEA_CREATURE_CHANCE("α"),
+    TRUE_DEFENSE("❂"),
+    DEFENSE("❈"),
+    FEROCITY("⫽"),
+    ABILITY_DAMAGE("๑"),
+    DAMAGE(""),
+
+    ;
+
+    private final String icon;
 
 }
