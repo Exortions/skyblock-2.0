23eab73e0724fa39c5717aaad199d774df636cd0
Exortions <exortionsmc@gmail.com>
2022-12-28 12:41:19 -0800

    add /hub and /is

---
+++ src/main/java/com/skyblock/skyblock/commands/player/HubCommand.java
@@ -0,0 +1,20 @@
+package com.skyblock.skyblock.commands.player;
+
+import com.skyblock.skyblock.Skyblock;
+import com.skyblock.skyblock.utilities.command.Command;
+import com.skyblock.skyblock.utilities.command.TrueAlias;
+import com.skyblock.skyblock.utilities.command.annotations.Description;
+import com.skyblock.skyblock.utilities.command.annotations.RequiresPlayer;
+import com.skyblock.skyblock.utilities.command.annotations.Usage;
+import org.bukkit.entity.Player;
+
+@RequiresPlayer
+@Usage(usage = "/hub")
+@Description(description = "Teleports you to the hub")
+public class HubCommand implements Command, TrueAlias<HubCommand> {
+
+    @Override
+    public void execute(Player player, String[] args, Skyblock plugin) {
+        player.performCommand("sb warp hub");
+    }
+}
---
+++ src/main/java/com/skyblock/skyblock/commands/player/IslandCommand.java
@@ -0,0 +1,21 @@
+package com.skyblock.skyblock.commands.player;
+
+import com.skyblock.skyblock.Skyblock;
+import com.skyblock.skyblock.utilities.command.Command;
+import com.skyblock.skyblock.utilities.command.TrueAlias;
+import com.skyblock.skyblock.utilities.command.annotations.Description;
+import com.skyblock.skyblock.utilities.command.annotations.RequiresPlayer;
+import com.skyblock.skyblock.utilities.command.annotations.Usage;
+import org.bukkit.entity.Player;
+
+@RequiresPlayer
+@Usage(usage = "/island")
+@Description(description = "Teleports you to your island")
+public class IslandCommand implements Command, TrueAlias<IslandCommand> {
+
+    @Override
+    public void execute(Player player, String[] args, Skyblock plugin) {
+        player.performCommand("sb warp home");
+    }
+
+}
