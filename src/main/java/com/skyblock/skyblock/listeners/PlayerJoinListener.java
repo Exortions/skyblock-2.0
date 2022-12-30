7c773356ca59527efe5d956e5da371598447f734
Exortions <exortionsmc@gmail.com>
2022-12-29 20:48:27 -0800

    remove join message

--- src/main/java/com/skyblock/skyblock/listeners/PlayerJoinListener.java
+++ src/main/java/com/skyblock/skyblock/listeners/PlayerJoinListener.java
@@ -2,12 +2,15 @@ package com.skyblock.skyblock.listeners;
 
 import com.skyblock.skyblock.features.island.IslandManager;
 import com.skyblock.skyblock.utilities.item.ItemBuilder;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemFlag;
+
+import java.util.UUID;
 
 public class PlayerJoinListener implements Listener {
 
@@ -20,6 +23,8 @@ public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void createIsland(PlayerJoinEvent event) {
+        event.setJoinMessage(null);
+
         IslandManager.createIsland(event.getPlayer());
     }
 
