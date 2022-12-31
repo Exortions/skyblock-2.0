eae8202a30fbc0f735bedcd2331ac6ba426d9758
Exortions <exortionsmc@gmail.com>
2022-12-30 18:44:32 -0800

    code style (redstone pigman)

--- src/main/java/com/skyblock/skyblock/features/entities/pigman/RedstonePigman.java
+++ src/main/java/com/skyblock/skyblock/features/entities/pigman/RedstonePigman.java
@@ -1,15 +1,7 @@
 package com.skyblock.skyblock.features.entities.pigman;
 
-import com.skyblock.skyblock.features.entities.EntityDrop;
-import com.skyblock.skyblock.features.entities.EntityDropRarity;
 import com.skyblock.skyblock.features.entities.SkyblockEntity;
-import com.skyblock.skyblock.utilities.Util;
-import org.bukkit.entity.*;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.craftbukkit.v1_8_R3.entity.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.bukkit.entity.EntityType;
 
 public class RedstonePigman extends SkyblockEntity {
     public RedstonePigman() {
