4e899be7c1c09790760ed32d49708b77d2312f95
OptimusChen <derek.hz.chen@gmail.com>
2023-01-01 21:09:53 -0800

    fix efficient axe

---
+++ src/main/java/com/skyblock/skyblock/event/SkyblockPlayerItemHeldChangeEvent.java
@@ -0,0 +1,16 @@
+package com.skyblock.skyblock.event;
+
+import com.skyblock.skyblock.SkyblockPlayer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.bukkit.inventory.ItemStack;
+
+@Getter
+@AllArgsConstructor
+public class SkyblockPlayerItemHeldChangeEvent extends SkyblockEvent {
+
+    private SkyblockPlayer player;
+    private ItemStack oldItem;
+    private ItemStack newItem;
+
+}
