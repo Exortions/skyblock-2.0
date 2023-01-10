9415fec64989bc3f1122c18ddb24cf213974e466
Exortions <exortionsmc@gmail.com>
2023-01-10 14:31:01 -0800

    start #151

--- src/main/java/com/skyblock/skyblock/features/entities/dragon/Dragon.java
+++ src/main/java/com/skyblock/skyblock/features/entities/dragon/Dragon.java
@@ -126,7 +126,7 @@ public class Dragon extends SkyblockEntity implements Listener {
     }
 
     private void fireBall(EnderDragon dragon) {
-        List<Player> players = Bukkit.getOnlinePlayers().stream().filter((p) -> SkyblockPlayer.getPlayer(p).getCurrentLocationName().equals("Dragon's Den")).collect(Collectors.toList());
+        List<Player> players = Bukkit.getOnlinePlayers().stream().filter((p) -> SkyblockPlayer.getPlayer(p).getCurrentLocationName().equals("Dragon's Nest")).collect(Collectors.toList());
 
         Player target = players.get(Util.random(0, players.size() - 1));
 
@@ -173,7 +173,7 @@ public class Dragon extends SkyblockEntity implements Listener {
     }
 
     private void lightningStrike() {
-        List<Player> players = Bukkit.getOnlinePlayers().stream().filter((p) -> SkyblockPlayer.getPlayer(p).getCurrentLocationName().equals("Dragon's Den")).collect(Collectors.toList());
+        List<Player> players = Bukkit.getOnlinePlayers().stream().filter((p) -> SkyblockPlayer.getPlayer(p).getCurrentLocationName().equals("Dragon's Nest")).collect(Collectors.toList());
 
         freezeFor(60);
 
@@ -199,7 +199,7 @@ public class Dragon extends SkyblockEntity implements Listener {
     }
 
     private void rush(EnderDragon dragon) {
-        List<Player> players = Bukkit.getOnlinePlayers().stream().filter((p) -> SkyblockPlayer.getPlayer(p).getCurrentLocationName().equals("Dragon's Den")).collect(Collectors.toList());
+        List<Player> players = Bukkit.getOnlinePlayers().stream().filter((p) -> SkyblockPlayer.getPlayer(p).getCurrentLocationName().equals("Dragon's Nest")).collect(Collectors.toList());
 
         Player target = players.get(Util.random(0, players.size() - 1));
 
@@ -230,7 +230,7 @@ public class Dragon extends SkyblockEntity implements Listener {
     protected void onDeath() {
         super.onDeath();
 
-        List<Player> players = Bukkit.getOnlinePlayers().stream().filter((p) -> SkyblockPlayer.getPlayer(p).getCurrentLocationName().equals("Dragon's Den")).collect(Collectors.toList());
+        List<Player> players = Bukkit.getOnlinePlayers().stream().filter((p) -> SkyblockPlayer.getPlayer(p).getCurrentLocationName().equals("Dragon's Nest")).collect(Collectors.toList());
         getDamaged().sort((p1, p2) -> Math.toIntExact(damagers.get(p1.getUniqueId()) - damagers.get(p2.getUniqueId())));
 
         for (Player p : players) {
