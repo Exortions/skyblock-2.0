0d9c993cc2e128b164518d8506ca1c4cb590baae
OptimusChen <derek.hz.chen@gmail.com>
2023-05-17 19:19:21 -0700

    rev slayer balancing changes

--- src/main/java/com/skyblock/skyblock/features/slayer/boss/RevenantHorror.java
+++ src/main/java/com/skyblock/skyblock/features/slayer/boss/RevenantHorror.java
@@ -84,15 +84,15 @@ public class RevenantHorror extends SlayerBoss {
 
         nms.getAttributeInstance(GenericAttributes.MOVEMENT_SPEED).setValue(getMovementSpeed());
 
-        if (tick % 50 == 0) {
-            skyblockPlayer.damage(getEntityData().damage, EntityDamageEvent.DamageCause.ENTITY_ATTACK, getVanilla());
+        if (tick % 80 == 0) {
+            skyblockPlayer.damage(getEntityData().damage / 2f, EntityDamageEvent.DamageCause.ENTITY_ATTACK, getVanilla());
         }
 
         if (tick % 20 == 0 && getLevel() >= 2) {
             for (Entity entity : getVanilla().getNearbyEntities(8.5, 5, 8.5)) {
                 if (entity instanceof Player) {
                     SkyblockPlayer p = SkyblockPlayer.getPlayer((Player) entity);
-                    p.damage(getEntityData().damage, EntityDamageEvent.DamageCause.ENTITY_ATTACK, getVanilla());
+                    p.damage(getEntityData().damage / 4f, EntityDamageEvent.DamageCause.ENTITY_ATTACK, getVanilla());
                 }
             }
         }
